0adb274d15eeaa5e700702fb967d661c8723e814
Kelly Wagemann <kelly.andrea97@gmail.com>
2018-10-09 17:02:23 -0300

    Remove DumpRequest from handler, create a function instead to compare request

--- client/client_test.go
+++ client/client_test.go
@@ -1,56 +1,51 @@
 package main
 
 import (
-  "net"
-  "net/http"
-  "net/http/httptest"
-  "net/url"
-  "testing"
-  "encoding/json"
-  "fmt"
-  "github.com/niclabs/testResolvers/config"
-  "net/http/httputil"
+	"net"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+	"encoding/json"
+	"github.com/niclabs/testResolvers/config"
+	"net/http/httputil"
 )
 
-func TestClientNoEncryption(t *testing.T) {
-  handler := func (rw http.ResponseWriter, req *http.Request) {
+func testMatchingRequest(reqServer *http.Request, reqClient *http.Request, body bool){
+	dumpServer, _:= httputil.DumpRequest(reqServer, body)
+	dumpClient, _:= httputil.DumpRequestOut(reqClient, body)
+	if(dumpServer!=dumpClient){ //find another way to make comparison, not valid.
+		t.Errorf("Request sent is different than request received")
+	}
+}
 
-    if req.URL.String() == "/get" {
-      rw.Header().Set("Content-Type", "application/json; charset=UTF-8")
-      rw.WriteHeader(http.StatusOK)
+func TestClientNoEncryption(t *testing.T){
+	handler := func (rw http.ResponseWriter, req *http.Request) {
+		if req.URL.String() == "/get" {
+			rw.Header().Set("Content-Type", "application/json; charset=UTF-8")
+			rw.WriteHeader(http.StatusOK)
+			err := json.NewEncoder(rw).Encode([]string {"1.2.3.4"})
+			if err != nil {
+				t.Errorf("Error marshaling list")
+			}
+			//var reqS *http.Request = req 
+    		}
+  	}
+	// local http server 
+	server := httptest.NewServer(http.HandlerFunc(handler))
+	defer server.Close()
+	// Now test the client, who receives a "configuration" file
+	var cfg config.Configuration = config.Configuration{}  
+	u,err := url.Parse(server.URL)
+	cfg.Server, cfg.Port, _ = net.SplitHostPort(u.Host)
 
-      err := json.NewEncoder(rw).Encode([]string {"1.2.3.4"})
-      if err != nil {
-        t.Errorf("Error marshaling list")
-      }
-    //Dumps ingoing request. Debugs client request.
-    dump, err := httputil.DumpRequest(req, true)
-		if err != nil {
-			http.Error(rw, fmt.Sprint(err), http.StatusInternalServerError)
-			return
-		}
+	var client Communication = REST {}
+	list, err  := client.Get(cfg)
 
-		fmt.Fprintf(rw, "%q", dump)
-    }
-  }
-  // local http server 
-  server := httptest.NewServer(http.HandlerFunc(handler))
-  defer server.Close()
-
-  // Now test the client, who receives a "configuration" file
-
-  var cfg config.Configuration = config.Configuration{}
-  
-  u,err := url.Parse(server.URL)
-  cfg.Server, cfg.Port, _ = net.SplitHostPort(u.Host)
-
-  var client Communication = REST {}
-  list, err  := client.Get(cfg)
-
-  if err != nil {
-    t.Errorf("Error in client.Get %s",err.Error())
-  }
-  if len(list) != 1 || list[0] != "1.2.3.4" {
-    t.Errorf("Message incorrect, got: %s, want: %s.",list,"1.2.3.4")
-  }
+	if err != nil {
+		t.Errorf("Error in client.Get %s",err.Error())
+	}
+	if len(list) != 1 || list[0] != "1.2.3.4" {
+		t.Errorf("Message incorrect, got: %s, want: %s.",list,"1.2.3.4")
+	}
 }
